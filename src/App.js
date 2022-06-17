1995236aa41d9f0307bfa598d02bec146e1e9216
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-18 00:23:04 +0600

    adding something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import ReactReducer from './useReducer/ReactReducer'
-// import Counter from './useReducer/Counter'
-import ComplexCounter from './useReducer/ComplexCounter';
+import Counter from './useReducer/Counter'
+// import ComplexCounter from './useReducer/ComplexCounter';
 function App() {
   return (
     <div >
      {/* <ReactReducer /> */}
-    {/* <Counter/> */}
-    <ComplexCounter/>
+    <Counter/>
+    {/* <ComplexCounter/> */}
     </div>
   );
 }
--- src/useReducer/Counter.js
+++ src/useReducer/Counter.js
@@ -1,29 +1,28 @@
-// // basic useReducer funciton 
-// import React,{useReducer} from 'react';
+// basic useReducer funciton
+import React,{useReducer} from 'react';
 
-// const initailState =0;
-// const reducer =(state, action)=>{
-//     switch(action){
-//         case 'increment':
-//             return state+1;
-//         case 'decrement':
-//             return state-1;
-//         default:
-//             return state;
-//     }
-// }
+const initailState =5;
+const reducer =(state, action)=>{
+    switch(action){
+        case 'increment':
+            return state+1;
+        case 'decrement':
+            return state-1;
+        default:
+            return state;
+    }
+}
 
-// const Counter = () => {
-//    const [count , dispatch]= useReducer(reducer, initailState);
-//     return (
-//         <div>
-//             <h1>the result is  {count}</h1>
-//             <button onClick={()=>dispatch('increment')}>increment</button>
-//             <button onClick={()=>dispatch('decrement')}>
-//             decrement</button>
+const Counter = () => {
+   const [count , dispatch]= useReducer(reducer, initailState);
+    return (
+        <div>
+            <h1>the result is  {count}</h1>
+            <button onClick={()=>dispatch('increment')}>increment</button>
+            <button onClick={()=>dispatch('decrement')}>decrement</button>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default Counter;
+export default Counter;
