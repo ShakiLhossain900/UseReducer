9aceec04fd6c4ee51e3e6035bac8146b4a9d29d0
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-17 23:11:13 +0600

    adding

--- src/useReducer/ComplexCounter.js
+++ src/useReducer/ComplexCounter.js
@@ -1,11 +1,29 @@
-import React from 'react';
+// redux use useReducer
+import React, { useReducer } from "react";
 
-const complexCounter = () => {
-    return (
-        <div>
-            <h1>this is useReducer complex counter</h1>
-        </div>
-    );
+const initailState = {
+  counter: 0,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { counter: state.counter + 1 };
+    case "decrement":
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
 };
 
-export default complexCounter;<h1>this is useReducer complex counter</h1>
+const ComplexCounter = () => {
+  const [count, dispatch] = useReducer(reducer, initailState);
+  return (
+    <div>
+      <h1>the result is = {count.counter}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement'})}>Decrement</button>
+    </div>
+  );
+};
+
+export default ComplexCounter;
--- src/useReducer/Counter.js
+++ src/useReducer/Counter.js
@@ -1,28 +1,29 @@
-import React,{useReducer} from 'react';
+// // basic useReducer funciton 
+// import React,{useReducer} from 'react';
 
-const initailState =0;
-const reducer =(state, action)=>{
-    switch(action){
-        case 'increment':
-            return state+1;
-        case 'decrement':
-            return state-1;
-        default:
-            return state;
-    }
-}
+// const initailState =0;
+// const reducer =(state, action)=>{
+//     switch(action){
+//         case 'increment':
+//             return state+1;
+//         case 'decrement':
+//             return state-1;
+//         default:
+//             return state;
+//     }
+// }
 
-const Counter = () => {
-   const [count , dispatch]= useReducer(reducer, initailState);
-    return (
-        <div>
-            <h1>the result is  {count}</h1>
-            <button onClick={()=>dispatch('increment')}>increment</button>
-            <button onClick={()=>dispatch('decrement')}>
-            decrement</button>
+// const Counter = () => {
+//    const [count , dispatch]= useReducer(reducer, initailState);
+//     return (
+//         <div>
+//             <h1>the result is  {count}</h1>
+//             <button onClick={()=>dispatch('increment')}>increment</button>
+//             <button onClick={()=>dispatch('decrement')}>
+//             decrement</button>
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default Counter;
+// export default Counter;
