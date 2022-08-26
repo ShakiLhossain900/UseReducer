84045f454b30175336dfbd347fc0ee43810f2617
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-26 22:52:31 +0600

    adding

--- src/useReducer/Practice.js
+++ src/useReducer/Practice.js
@@ -23,31 +23,35 @@
 
 
 
-import React,{ useReducer } from 'react';
+// import React,{ useReducer } from 'react';
 
-const initailState = 0;
+// const initailState = 0;
 
-const reducerFunction = (state, action)=>{
-      switch(action){
-        case 'increment':
-            return state+1;
-        case 'decrement':
-            return state-1;
-        default:
-            return state;
-      }
-}
+// const reducerFunction = (state, action)=>{
+//       switch(action){
+//         case 'increment':
+//             return state+1;
+//         case 'decrement':
+//             return state-1;
+//         default:
+//             return state;
+//       }
+// }
 
 
-const Practice = () => {
-    const [count,dispatch] =useReducer(reducerFunction,5);
-    return (
-        <div>
-            <h1>the resut is = {count}</h1>
-            <button onClick={()=>dispatch('increment')} >Increment</button> 
-            <button onClick={()=>dispatch('decrement')} >decrement</button>
-        </div>
-    );
-};
+// const Practice = () => {
+//     const [count,dispatch] =useReducer(reducerFunction,5);
+//     return (
+//         <div>
+//             <h1>the resut is = {count}</h1>
+//             <button onClick={()=>dispatch('increment')} >Increment</button> 
+//             <button onClick={()=>dispatch('decrement')} >decrement</button>
+//         </div>
+//     );
+// };
 
-export default Practice;
+// export default Practice;
+
+
+
+//useReducer akta object use kore 
